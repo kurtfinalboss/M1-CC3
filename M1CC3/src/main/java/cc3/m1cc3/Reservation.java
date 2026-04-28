a48ed06cfa9e5d89baad350a359d076a01c7d7e2
JakeDanielConsorte <jakedaniel.consorte@gmail.com>
2026-04-28 22:38:13 +0800

    Refactor Reservation class to use Builder pattern

--- M1CC3/src/main/java/cc3/m1cc3/Reservation.java
+++ M1CC3/src/main/java/cc3/m1cc3/Reservation.java
@@ -1,93 +1,72 @@
-package cc3.m1cc3;
-
 import java.util.Random;
-import java.util.Scanner;
 
 public class Reservation {
-    private final Scanner sc = new Scanner(System.in);
     private final String RESERVATION_CODE;
     private final Passenger PASSENGER;
     private final Route ROUTE;
+    private final Fare FARE;
+    private final Services SERVICES;
+    private final int SEAT_NUMBER;
+    private final double TOTAL_FARE;
 
     private Reservation(){
         this.RESERVATION_CODE = null;
         this.PASSENGER = null;
         this.ROUTE = null;
+        this.FARE = null;
+        this.SERVICES = null;
+        this.SEAT_NUMBER = 0;
+        this.TOTAL_FARE = 0.0;
     }
-    
-    private Reservation(ReservationBuilder builder ){
-        this.RESERVATION_CODE = generateReservationCode();
+    private Reservation(ReservationBuilder builder){
+        if(builder.passenger == null) throw new IllegalStateException("Passenger required!");
+        this.RESERVATION_CODE = "TK-" + (new Random().nextInt(900) + 100);
         this.PASSENGER = builder.passenger;
         this.ROUTE = builder.route;
+        this.FARE = builder.fare;
+        this.SERVICES = builder.services;
+        this.SEAT_NUMBER = builder.seatNumber;
+        this.TOTAL_FARE = builder.totalFare;
     }
     
     public String getReservationCode(){ return RESERVATION_CODE;}
     public Passenger getPassenger(){ return PASSENGER;}
     public Route getRoute(){ return ROUTE;}
+    public Fare getFare(){ return FARE;}
+    public Services getServices(){ return SERVICES;}
+    public int getSeatNumber(){ return SEAT_NUMBER; }
+    public double getTotalFare(){ return TOTAL_FARE; }
     
-    public void reservationConfirmed(){
+    
+    public void confirmReservation() {
+        System.out.println("\n===============================================");
+        System.out.println("#             RESERVATION SUMMARY             #");
+        System.out.println("===============================================");
+        System.out.printf("%-18s : %s%n", "Full Name", PASSENGER.getFullname());
+        System.out.printf("%-18s : %s%n", "Contact Number", PASSENGER.getContactNumber());
+        System.out.printf("%-18s : %s%n", "Category", FARE.getPassengerCategory());
+        System.out.printf("%-18s : %s%n", "Discount", FARE.getDiscountRate()* 100 + "%");
+        System.out.printf("%-18s : %s%n", "Origin", ROUTE.getOriginStation());
+        System.out.printf("%-18s : %s%n", "Destination", ROUTE.getDestinationStation());
+        System.out.printf("%-18s : %s%n", "Departure Time", ROUTE.getDepartureTime());
+        System.out.printf("%-18s : %s%n", "Date", ROUTE.getReservationDate());
+        System.out.println("===============================================");
+    }
+    
+    public void reservationConfirmed() {
         System.out.println("\n|****************************************************************|");
         System.out.printf("%43s%n", "CONFIRMED RESERVATION");
         System.out.println("|****************************************************************|");
         System.out.println("Reservation confirmed! \nYour reference number: " + RESERVATION_CODE);
-    }   
-    
-    public boolean cancelReservation() {
-        while (true) {
-            System.out.println("\nDo you want to cancel your reservation?");
-            System.out.println("[1] Confirm");
-            System.out.println("[0] Cancel");
-            System.out.print("Enter Choice: ");
-
-            if (sc.hasNextInt()) {
-                int choice = sc.nextInt();
-                sc.nextLine();
-
-                if (choice == 1) {
-                    System.out.println("\n*SUCCESS!* You have cancelled your reservation.");
-                    return true;
-                } 
-                else if (choice == 0) {
-                    System.out.println("\nYour reservation is still active.");
-                    return false; 
-                } 
-                else {
-                    System.out.println("\n*INVALID INPUT!* Please enter a valid option.");
-                }
-
-            } else {
-                System.out.println("\n*INVALID INPUT!* Please enter a number only.");
-                sc.next();
-            }
-        }
-}
-    
-    private String generateReservationCode(){
-        Random rand = new Random();
-        int num = rand.nextInt(1000);
-        return String.format("TK-%03d", num);
-    }
-     
-     public void confirmReservation(){
-         
-        Reservation reserve = new Reservation.ReservationBuilder().setPassenger(PASSENGER).setRoute(ROUTE).build();
-        System.out.println("\n===============================================");
-        System.out.println("#             RESERVATION SUMMARY             #");
-        System.out.println("===============================================");
-        System.out.printf("%-18s : %s%n", "Full Name", reserve.getPassenger().getFullname());
-        System.out.printf("%-18s : %s%n", "Contact Number", reserve.getPassenger().getContactNumber());
-        System.out.printf("%-18s : %s%n", "Category", reserve.getPassenger().getPassengerCategory());
-        System.out.printf("%-18s : %s%n", "Discount", reserve.getPassenger().getDiscountRate()* 100 + "%");
-        System.out.printf("%-18s : %s%n", "Origin", reserve.getRoute().getOriginStation());
-        System.out.printf("%-18s : %s%n", "Destination", reserve.getRoute().getDestinationStation());
-        System.out.printf("%-18s : %s%n", "Departure Time", reserve.getRoute().getDepartureTime());
-        System.out.println("===============================================");
-     }
-    
+    }  
     public static class ReservationBuilder{
         private String reservationCode;
         private Passenger passenger;
         private Route route;
+        private Fare fare;
+        private Services services;
+        private int seatNumber;
+        private double totalFare;
         
         public ReservationBuilder setPassenger(Passenger passenger){
             this.passenger = passenger;
@@ -96,6 +75,26 @@ public class Reservation {
         
         public ReservationBuilder setRoute(Route route){
             this.route = route;
+            return this;
+        }
+        
+        public ReservationBuilder setFare(Fare fare){
+            this.fare = fare;
+            return this;
+        }
+        
+        public ReservationBuilder setServices(Services services){
+            this.services = services;
+            return this;
+        }
+        
+        public ReservationBuilder setSeatNumber(int seatNumber){
+            this.seatNumber = seatNumber;
+            return this;
+        }
+
+        public ReservationBuilder setTotalFare(double totalFare){
+            this.totalFare = totalFare;
             return this;
         }
         
